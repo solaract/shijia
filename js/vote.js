c4112ce6abe227aee1b0d0759279bad6bb8d1bb2
zxy <304246725@qq.com>
2015-11-18 20:55:52 +0800

    投票部分更改

--- js/vote.js
+++ js/vote.js
@@ -4,14 +4,24 @@
 ;(function($){
 
     $(document).ready(function(){
+        //定位分页
         var _hash = document.location.hash;
         if(!_hash){
             return;
         }
         var _$nav = $("[href*="+_hash+"]").children();
-        console.log(_hash);
+//        console.log(_hash);
         _$nav.addClass('vote_nav_li_sel');
         switch_page(_$nav);
+
+        //禁用已投票表单
+        $('.vote_form').each(function(){
+            var _$form = $(this);
+            if(_$form.data('voted')){
+                _$form.find('.vote_v').addClass('vote_ved').html('&#xe660;');
+                _$form.find('.vote_sub').addClass('vote_subed').val('已投票');
+            }
+        })
     });
 
     //vote切换分页
@@ -33,7 +43,87 @@
         switch_page($(this));
     });
 //    switch_page($('.vote_nav_li'));
+    //投票部分
+    var $vote = $('.vote_v');
+    $vote.on('mouseenter',function(){
+        if($(this).next().prop('checked')||$(this).parents('form').data('voted')){
+            return false;
+        }
+        $(this).css({
+            width:'30px',
+            height:'30px',
+            right:'0'
+        });
+    });
+    $vote.on('mouseleave',function(){
+        if($(this).next().prop('checked')||$(this).parents('form').data('voted')){
+            return false;
+        }
+        $(this).css({
+            width:'20px',
+            height:'20px',
+            right:'5px'
+        });
+    });
+    $vote.on('click',function(){
+        var _$vote = $(this),
+            _$check = _$vote.next();
+        if(_$vote.parents('form').data('voted')){
+            return false;
+        }
+        if(_$check.prop('checked')){
+            _$vote.css({
+                borderWidth:'2px',
+                right:'0px'
+            }).html('');
+            _$check.prop('checked',false);
+        }
+        else{
+            _$vote.css({
+                borderWidth:'0px',
+                width:'30px',
+                height:'30px',
+                right:'2px'
+            }).html('&#xe660;');
+            _$check.prop('checked',true);
+        }
+    });
+    $('.vote_form').on('submit',function(){
+        var _$form = $(this),
+            _checked = [],
+            _obj = {},
+            _type;
+        if(_$form.data('voted')){
+            return false;
+        }
+        _$form.find('.vote_v_in').each(function(){
+            var _$in = $(this);
+            if(_$in.prop('checked')){
+                _checked.push(_$in.val());
+            }
+        });
+        var _len = _checked.length;
+        if(_len === 0){
+            alert('请先选择候选人再投票');
+            return false;
+        }
+        if(_len>6&&_len<11){
+            _type = _$form.find('.vote_v_in').prop('name');
+            _obj.type = _type;
+            _obj.checked = _checked;
+            _obj = JSON.stringify(_obj);
+//            alert(1);
+//            $.ajax()
 
+//            _checked = JSON.stringify(_checked);
+            console.log(typeof _obj,_obj,JSON.parse(_obj));
+        }else{
+            alert('每次只能投给7-10位候选人哦');
+            return false;
+        }
+//        console.log(this);
+        return false;
+    });
 
 
 
